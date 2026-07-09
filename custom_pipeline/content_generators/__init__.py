20a37bfc3c49a1a4ef252f7830139d5757be93c3
erwinimron <79198310+erwinimron@users.noreply.github.com>
2026-07-09 16:19:58 +0700

    Update __init__.py

--- custom_pipeline/content_generators/__init__.py
+++ custom_pipeline/content_generators/__init__.py
@@ -1,4 +1,5 @@
 # custom_pipeline/content_generators/__init__.py
 from .storytelling import StorytellingGenerator
+from .infographic import InfographicGenerator
 
-__all__ = ['StorytellingGenerator']
+__all__ = ['StorytellingGenerator', 'InfographicGenerator']
