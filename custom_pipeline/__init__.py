b5ca6a1cb642f07bbdf1af47e9ed800693203ba7
erwinimron <79198310+erwinimron@users.noreply.github.com>
2026-07-09 16:32:41 +0700

    Update __init__.py

--- custom_pipeline/__init__.py
+++ custom_pipeline/__init__.py
@@ -1,5 +1,11 @@
-# custom_pipeline/__init__.py
+# Nanti kalau sudah punya banyak generator
 from .pipeline import ContentPipeline
 from .spreadsheet_manager import SpreadsheetManager
+from .content_generators import StorytellingGenerator, InfographicGenerator
 
-__all__ = ['ContentPipeline', 'SpreadsheetManager']
+__all__ = [
+    'ContentPipeline', 
+    'SpreadsheetManager',
+    'StorytellingGenerator',
+    'InfographicGenerator'
+]
